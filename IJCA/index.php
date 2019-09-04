657e9c9d4f9eca400557ced9f1b2cb3ab519cec5
Julia Valerio <juliav286@gmail.com>
2019-09-04 14:48:47 -0300

    primeira modificação - navbar - responsive

---
+++ IJCA/index.php
@@ -0,0 +1,45 @@
+<html class=" js_active  vc_desktop  vc_transform  vc_transform "><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="http://www.ijca.org.br/pt-br/feed/">
+    <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="http://www.ijca.org.br/pt-br/feed/atom/">
+	<title>IJCA - Instituto Jelson da Costa Antunes</title>
+
+
+<body>
+
+	<header id="header" class="full">
+            <input type="checkbox" id="control-nav" />
+            <label for="control-nav" class="control-nav"></label>
+            <label for="control-nav" class="control-nav-close"></label>
+            
+		<nav id="navegation" class="row">
+			
+			<h1>
+				<a href="http://www.ijca.org.br/pt-br">
+					<i id="logo">IJCA</i>
+				</a>
+            </h1>
+					
+		<ul id="menu-linguagens" class="nav-language"><li id="menu-item-22" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22"><a href="#">PT</a></li>
+            <li><a href="#">EN</a></li>
+        </ul>			
+		<ul id="menu-redes-sociais" class="nav-social"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-94"><a target="_blank" href="https://www.instagram.com/instituto.jca/?hl=pt-br"><i class="icon-instagram"></i></a></li>
+            <li><a target="_blank" href="https://www.flickr.com/photos/114395556@N05/"><i class="icon-flickr"></i></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/InstitutoJca/"><i class="icon-facebook"></i></a></li>
+            <li><a target="_blank" href="https://www.youtube.com/channel/UCwesHw0prt17nEyyzVEcXaw"><i class="icon-youtube"></i></a></li>
+        </ul>	            		
+
+        <ul id="menu-principal" class="nav-main">
+            <li ><a href="http://www.ijca.org.br/pt-br/quem-somos/">Quem somos</a></li>
+            <li><a href="http://www.ijca.org.br/pt-br/atuaca>Atuação">Atuação</a></li>
+            <li><a href="http://www.ijca.org.br/pt-br/linha-do-tempo/">Linha do tempo</a></li>
+            <li><a href="http://www.ijca.org.br/pt-br/noticias/">Notícias</a></li>
+            <li><a href="http://www.ijca.org.br/pt-br/publicacoes/">Publicações</a></li>
+            <li><a href="http://www.ijca.org.br/pt-br/contato/">Contato</a></li>
+        </ul>	
+			</nav>
+		</header>
+
+</body></html>
